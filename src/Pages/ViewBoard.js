f89dbe495c01191326191de81d36c371d90c1c8b
hasib1224 <bsse1224@iit.du.ac.bd>
2023-09-05 21:51:51 +0600

    the circle problem is solved but same color

--- src/Pages/ViewBoard.js
+++ src/Pages/ViewBoard.js
@@ -1,203 +1,202 @@
-import React, { useEffect, useState } from 'react';
-import './board.css';
+import React, { useEffect, useState } from "react";
+import "./board.css";
 // import { main } from '../Board/Move.js';
-import { AI_movement } from '../Board/Move.js';
-
+import { AI_movement } from "../Board/Move.js";
 
 const Board = () => {
-	const row_count = 10;
-	const col_count = 10;
-	
-	const [turn, setTurn] = useState('0');
-	const [cells, setCells] = useState(Array(row_count*col_count).fill(''));
-	const [winner, setWinner] = useState('');
+  const row_count = 10;
+  const col_count = 10;
 
-	const handleClick = (num) => {
-		console.log(cells)
-		if (cells[num] !== '') {
-			alert('already clicked');
-			return;
-		}
+  const [turn, setTurn] = useState("0");
+  const [cells, setCells] = useState(Array(row_count * col_count).fill(""));
+  const [winner, setWinner] = useState("");
 
-		let squares = [...cells];
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("already clicked");
+      return;
+    }
 
-		//if (turn === '0') {
-			squares[num] = '0';
-			setCells(squares)
-			
-			setTurn('1');
-		// } else {
-		// 	squares[num] = '1';
-		// 	setTurn('0');
-		// }
-		console.log(squares," ------------ forntent")
-		
-		setCells(squares);
-		const cell = AI_movement(squares);
-		console.log(cell,"++++++++++++++++++ in the forntent after ai movement")
-		setCells(cell);
+    let squares = [...cells];
 
-		setTurn('1');
+    if (turn === "0") {
+      squares[num] = "0";
+      setCells(squares);
 
-		
-	};
+      setTurn("1");
+    } else {
+      squares[num] = "1";
+      setTurn("0");
+    }
+    console.log(squares, " ------------ forntent");
 
-	const handleRestart = () => {
-		setWinner('');
-		setCells(Array(row_count*col_count).fill(''));
-		setTurn('0');
-	};
+    setCells(squares);
+    const cell = AI_movement(squares);
+    console.log(cell, "++++++++++++++++++ in the forntent after ai movement");
+    setCells(cell);
 
-	const Cell = ({ num }) => {
-		return <td onClick={() => handleClick(num)}><div className={cells[num]} /></td>;
-	};
+    setTurn("1");
+  };
 
-	useEffect(() => {
-		setTimeout(() => {
-			if(turn === '1'){
-				let squares = [...cells]
-				
-			}
-		}, 0)
-	})
+  const Cell = ({ num }) => {
+	const className = `c${cells[num]}`;
+    return (
+      <td onClick={() => handleClick(num)}>
+        <div className={className} />
+      </td>
+    );
+  };
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (turn === "1") {
+        let squares = [...cells];
+      }
+    }, 0);
+  });
 
-    var t = 0;
+  var t = 0;
 
-	return (
-		<div className='container mt-5'>
-			{turn==='1' && (
-				<b><p><span class="red">Red</span>'s Turn!</p></b>
-			)}
-			{turn==='0' && (
-				<b><p>Your Turn!</p></b>
-			)}
-			<table className="box">
-				<tbody>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-					<tr>
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-						<Cell num={t++} />
-					</tr>
-				</tbody>
-			</table>
-			</div>
-	);
+  return (
+    <div className="container mt-5">
+      {turn === "1" && (
+        <b>
+          <p>
+            <span class="red">Red</span>'s Turn!
+          </p>
+        </b>
+      )}
+      {turn === "0" && (
+        <b>
+          <p>Your Turn!</p>
+        </b>
+      )}
+      <table className="box">
+        <tbody>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+          <tr>
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+            <Cell num={t++} />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Board;
---
+++ src/Pages/habijabi.js
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from 'react';
+import './board.css';
+import { comMove } from '../ComMove';
+import { checkForWinner } from './readboard';
+
+const Board = () => {
+	const row_count = 10;
+	const col_count = 10;
+	//const win_count = 5;
+	
+	const [turn, setTurn] = useState('b');
+	const [cells, setCells] = useState(Array(row_count*col_count).fill(''));
+	const [winner, setWinner] = useState('');
+
+	const handleClick = (num) => {
+		if (cells[num] !== '') {
+			alert('already clicked');
+			return;
+		}
+
+		let squares = [...cells];
+
+		if (turn === 'b') {
+			squares[num] = 'b';
+			setCells(squares)
+			//boardlines(squares);
+			// comMove(squares);
+			// setTurn('b');
+			setTurn('r');
+		} else {
+			squares[num] = 'r';
+			setTurn('b');
+			// boardlines(squares);
+		}
+		//console.log(cells);
+		setCells(squares);
+		setWinner(checkForWinner(squares));
+	};
+
+
+	const Cell = ({ num }) => {
+		return <td onClick={() => handleClick(num)}><div className={cells[num]} /></td>;
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			if(turn === 'r'){
+				let squares = [...cells]
+				let best = comMove(squares)
+				handleClick(best);
+			}
+		}, 0)
+	})
+
+    var t = 0;
+
+	return (
+		<div className='container mt-5'>
+			{turn==='r' && (
+				<b><p><span class="red">Red</span>'s Turn!</p></b>
+			)}
+			{turn==='b' && (
+				<b><p>Your Turn!</p></b>
+			)}
+			<table className="box">
+				<tbody>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+				</tbody>
+			</table>	
+		</div>
+	);
+};
+
+export default Board;
