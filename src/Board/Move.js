84984c266a712e0566cfe54d93b8f0925db1bb8f
hasib1224 <bsse1224@iit.du.ac.bd>
2023-09-08 17:53:32 +0600

    no need to use move.js file

--- src/Board/Move.js
+++ src/Board/Move.js
@@ -1,5 +1,5 @@
-import {boardEval as evaluateBoard} from "./boardEvaluation.js";
-import {findBestMove as findBestMove2} from "./Minimax.js";
+// import {boardEval as evaluateBoard} from "./boardEvaluation.js";
+// import {findBestMove as findBestMove2} from "./Minimax.js";
 
 const SIZE = 10;
 const AI = "1";
@@ -7,73 +7,73 @@ const OPPONENT = "O";
 const EMPTY = "";
 
 
-// function evaluateLine(line, player) {
-//   const countAICell = line.filter((cell) => cell === player).length;
-//   const countOpponentCell = line.filter((cell) => cell === OPPONENT).length;
+function evaluateLine(line, player) {
+  const countAICell = line.filter((cell) => cell === player).length;
+  const countOpponentCell = line.filter((cell) => cell === OPPONENT).length;
 
-//   // if (countAICell === 6)
-//   //     return 2000;
-//   if (countAICell === 5) return 1000;
-//   if (countOpponentCell === 5) return -1000;
-//   if (countAICell === 4 && line.includes(EMPTY)) return 100;
-//   if (countOpponentCell === 4 && line.includes(EMPTY)) return -100;
-//   if (countAICell === 3 && line.includes(EMPTY)) return 10;
-//   if (countOpponentCell === 3 && line.includes(EMPTY)) return -10;
-//   if (countAICell === 2 && line.includes(EMPTY)) return 1;
-//   if (countOpponentCell === 2 && line.includes(EMPTY)) return -1;
+  // if (countAICell === 6)
+  //     return 2000;
+  if (countAICell === 5) return 1000;
+  if (countOpponentCell === 5) return -1000;
+  if (countAICell === 4 && line.includes(EMPTY)) return 100;
+  if (countOpponentCell === 4 && line.includes(EMPTY)) return -100;
+  if (countAICell === 3 && line.includes(EMPTY)) return 10;
+  if (countOpponentCell === 3 && line.includes(EMPTY)) return -10;
+  if (countAICell === 2 && line.includes(EMPTY)) return 1;
+  if (countOpponentCell === 2 && line.includes(EMPTY)) return -1;
 
-//   return 0;
-// }
+  return 0;
+}
 
 
 
 
 // Evaluate the entire board        @update don't check the row and column if there emty skip it
-// function evaluateBoard(board, player) {
+function evaluateBoard(board, player) {
 
-//   let totalScore = 0;
+  let totalScore = 0;
 
-//   for (const row of board) {
-//     for (let i = 0; i <= SIZE - 5; i++) {
-//       totalScore += evaluateLine(row.slice(i, i + 5), player);
-//     }
-//   }
+  for (const row of board) {
+    for (let i = 0; i <= SIZE - 5; i++) {
+      totalScore += evaluateLine(row.slice(i, i + 5), player);
+    }
+  }
 
-//   for (let col = 0; col < SIZE; col++) {
-//     for (let i = 0; i <= SIZE - 5; i++) {
-//       if (board[i][col] != EMPTY) {
-//         const column = Array.from({ length: 5 }, (_, j) => board[i + j][col]);
-//         totalScore += evaluateLine(column, player);
-//       }
-//     }
-//   }
+  for (let col = 0; col < SIZE; col++) {
+    for (let i = 0; i <= SIZE - 5; i++) {
+      if (board[i][col] != EMPTY) {
+        const column = Array.from({ length: 5 }, (_, j) => board[i + j][col]);
+        totalScore += evaluateLine(column, player);
+      }
+    }
+  }
 
-//   for (let i = 0; i <= SIZE - 5; i++) {
-//     for (let j = 0; j <= SIZE - 5; j++) {
-//       if (board[i][j] != EMPTY) {
-//         const diagonal = Array.from(
-//           { length: 5 },
-//           (_, k) => board[i + k][j + k]
-//         );
-//         totalScore += evaluateLine(diagonal, player);
-//       }
-//     }
-//   }
+  for (let i = 0; i <= SIZE - 5; i++) {
+    for (let j = 0; j <= SIZE - 5; j++) {
+      if (board[i][j] != EMPTY) {
+        const diagonal = Array.from(
+          { length: 5 },
+          (_, k) => board[i + k][j + k]
+        );
+        totalScore += evaluateLine(diagonal, player);
+      }
+    }
+  }
 
-//   for (let i = 0; i <= SIZE - 5; i++) {
-//     for (let j = 0; j <= SIZE - 5; j++) {
-//       if (board[i][j] != EMPTY) {
-//         const diagonal = Array.from(
-//           { length: 5 },
-//           (_, k) => board[i + k][j + 4 - k]
-//         );
-//         totalScore += evaluateLine(diagonal, player);
-//       }
-//     }
-//   }
+  for (let i = 0; i <= SIZE - 5; i++) {
+    for (let j = 0; j <= SIZE - 5; j++) {
+      if (board[i][j] != EMPTY) {
+        const diagonal = Array.from(
+          { length: 5 },
+          (_, k) => board[i + k][j + 4 - k]
+        );
+        totalScore += evaluateLine(diagonal, player);
+      }
+    }
+  }
 
-//   return totalScore;
-// }
+  return totalScore;
+}
 
 
 
